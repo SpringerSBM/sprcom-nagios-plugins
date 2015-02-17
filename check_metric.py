2581318df97bf93e7bfa7ad14c5518d4facc5d8a
Eric Schwarzkopf <eric.schwarzkopf@springer.com>
2015-02-17 09:51:17 +0100

    Added verbose logging. Improved help.

--- check_metric.py
+++ check_metric.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python
+# coding=utf-8
 
 """Retrieves and checks a metric from graphite."""
 
+from __future__ import print_function
+
 __version__ = "1.0"
 
+import sys
 import argparse
 import requests
 import nagiosplugin
 
+# are we in verbose logging mode?
+verbose = False
 
 @nagiosplugin.guarded
 class Graphite(nagiosplugin.Resource):
@@ -27,17 +33,29 @@ class Graphite(nagiosplugin.Resource):
 
 def retrieve_metric(host, name):
     params = {"target": name, "format": "json", "from": "-5min"}
-    return requests.get("http://%s/render" % host, params=params).json()
+    r = requests.get("http://%s/render" % host, params=params)
+    if verbose:
+        print("### sent: %s" % r.request.url, file=sys.stderr)
+        print("### received status: %s" % r.status_code, file=sys.stderr)
+    return r.json()
 
+
+range_desc = """
+supports nagios-style ranges. The general format is “[@][start:][end]”. “start:” may be omitted if start==0.
+“~:” means that start is negative infinity. If end is omitted, infinity is assumed.
+To invert the match condition, prefix the range expression with “@”.
+"""
 
 o = argparse.ArgumentParser(description=__doc__)
-
 o.add_argument('-m', '--metric', help="name of the graphite metric to check", required=True)
 o.add_argument('-H', '--host', help="name of the graphite host", required=True)
-o.add_argument('-w', '--warning', metavar='RANGE', help="(supports nagios-style ranges)", required=True)
-o.add_argument('-c', '--critical', metavar='RANGE', help="(supports nagios-style ranges)", required=True)
+o.add_argument('-w', '--warning', metavar='RANGE', help=range_desc, required=True)
+o.add_argument('-c', '--critical', metavar='RANGE', help=range_desc, required=True)
 o.add_argument('-V', '--version', action="version", version=__version__)
+o.add_argument('-v', '--verbose', action="store_true", help="increase output verbosity")
 options = o.parse_args()
 
+# noinspection PyRedeclaration
+verbose = options.verbose
 nagiosplugin.Check(Graphite(options.host, options.metric),
                    nagiosplugin.ScalarContext("Graphite", options.warning, options.critical)).main()
