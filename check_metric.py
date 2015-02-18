58e6521825e8e8a2be0c8009e2c7263eb6f6f266
Eric Schwarzkopf <eric.schwarzkopf@springer.com>
2015-02-18 10:06:46 +0100

    Added time-interval parameters.

--- check_metric.py
+++ check_metric.py
@@ -17,12 +17,14 @@ verbose = False
 
 @nagiosplugin.guarded
 class Graphite(nagiosplugin.Resource):
-    def __init__(self, host, metric):
+    def __init__(self, host, metric, start_time, end_time):
         self.host = host
         self.metric = metric
+        self.start_time = start_time
+        self.end_time = end_time
 
     def probe(self):
-        metric_json = retrieve_metric(self.host, self.metric)
+        metric_json = retrieve_metric(self.host, self.metric, self.start_time, self.end_time)
         for data in metric_json:
             for point in reversed(data['datapoints']):
                 if point[0] is not None:
@@ -31,8 +33,8 @@ class Graphite(nagiosplugin.Resource):
         return []
 
 
-def retrieve_metric(host, name):
-    params = {"target": name, "format": "json", "from": "-5min", "until": "-90s"}
+def retrieve_metric(host, name, start_time, end_time):
+    params = {"target": name, "format": "json", "from": start_time, "until": end_time}
     r = requests.get("http://%s/render" % host, params=params)
     if verbose:
         print("### sent: %s" % r.request.url, file=sys.stderr)
@@ -51,11 +53,13 @@ o.add_argument('-m', '--metric', help="name of the graphite metric to check", re
 o.add_argument('-H', '--host', help="name of the graphite host", required=True)
 o.add_argument('-w', '--warning', metavar='RANGE', help=range_desc, required=True)
 o.add_argument('-c', '--critical', metavar='RANGE', help=range_desc, required=True)
+o.add_argument('-s', '--start', help="beginning of the time interval within which data is to be retrieved", default="-5min")
+o.add_argument('-e', '--end', help="end of the time interval within which data is to be retrieved", default="-90s")
 o.add_argument('-V', '--version', action="version", version=__version__)
 o.add_argument('-v', '--verbose', action="store_true", help="increase output verbosity")
 options = o.parse_args()
 
 # noinspection PyRedeclaration
 verbose = options.verbose
-nagiosplugin.Check(Graphite(options.host, options.metric),
+nagiosplugin.Check(Graphite(options.host, options.metric, options.start, options.end),
                    nagiosplugin.ScalarContext("Graphite", options.warning, options.critical)).main()
